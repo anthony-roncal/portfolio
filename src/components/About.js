c120abf3f05747c9fff099a76593a7cf5a7b0478
Anthony <acr1191@gmail.com>
2023-07-20 11:59:03 -0700

    update about summary

--- src/components/About.js
+++ src/components/About.js
@@ -15,7 +15,7 @@ const About = () => {
                     Dedicated Quality Engineer with a strong foundation in software testing, now leveraging Technical expertise and problem-solving skills 
                     to pursue a career as a Front-End Developer. Adept at collaborating with cross-functional Agile teams to deliver high-quality solutions, 
                     now applying proficiency in HTML, CSS, and JavaScript to craft engaging and user-friendly web experiences. Eager to contribute 
-                    innovative solutions and meticulous attention to detail in the field of front-end development.
+                    innovative solutions and meticulous attention to detail in the field of front-end development. 
                 </p>
                 <IconLinks />
             </div>
